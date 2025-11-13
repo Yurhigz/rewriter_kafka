62cd3c2c0f5d36ad37d0d8cd2d590cd4ea132530
Benoit <90827157+Yurhigz@users.noreply.github.com>
2025-11-13 15:35:26 +0100

    Optimize JSON timestamp transformation
    Optimize timestamp calculation and JSON transformation logic.

--- proccesing/jsontransforming.go
+++ proccesing/jsontransforming.go
@@ -8,11 +8,13 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
+// Les jours disponibles sont soit le 22 ou le 23
+const dayNano = 24 * 60 * 60 * 1000000000
+
 func transformMessageOptimized(m kafka.Message) kafka.Message {
 	origJSON := m.Value
 	newJSON := make([]byte, 0, len(origJSON)+1024)
 
-	// Calculer une seule fois le timestamp de base pour aujourd'hui
 	now := time.Now()
 	todayBase := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	todayBaseNano := todayBase.UnixNano()
@@ -21,7 +23,6 @@ func transformMessageOptimized(m kafka.Message) kafka.Message {
 		if err != nil {
 			return
 		}
-
 		ts, err := jsonparser.GetInt(value, "timestamp")
 		if err != nil {
 			return
@@ -32,19 +33,22 @@ func transformMessageOptimized(m kafka.Message) kafka.Message {
 			int64(origTime.Minute())*60000000000 +
 			int64(origTime.Second())*1000000000 +
 			int64(origTime.Nanosecond())
+		origTimeDay := origTime.Day()
+		var newTimestamp int64
 
-		newTimestamp := todayBaseNano + timeInDay
+		offsetDay := -1
+		if origTimeDay == 23 {
+			offsetDay = 0
+		}
+		newTimestamp = todayBaseNano + int64(offsetDay)*int64(dayNano) + timeInDay
 
-		// Conversion rapide du timestamp
 		tsBytes := []byte(fmt.Sprintf("%d", newTimestamp))
 
-		// Remplacer le timestamp
 		modifiedValue, err := jsonparser.Set(value, tsBytes, "timestamp")
 		if err != nil {
 			return
 		}
 
-		// Construire le JSON final
 		if len(newJSON) == 0 {
 			newJSON = append(newJSON, '[')
 		} else {
